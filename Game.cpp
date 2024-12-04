5b5ee8f0ff26f1b02883ba495db07c93e48ec5fb
kou0099 <hasako10@icloud.com>
2024-12-04 13:38:42 +0900

    色々やった

--- Game.cpp
+++ Game.cpp
@@ -6,31 +6,47 @@
 #include "Keyboard.h"
 #include "Audio.h"
 
-int YukaX = YUKA_X; // 現在のYUKA_X
-int YukaY = YUKA_Y; // 現在のYUKA_Y
+int YokoX = YOKO_X; // 現在の地面の横幅
+int YokoY = YOKO_Y; // 現在の地面の高さ
+int TateX = TATE_X;
+int TateY = TATE_Y;
 void Game::Init(HWND hWnd) {
 	D3D_Create(hWnd);
 	srand(GetTickCount());
-	//sound.Init();
-	//sound.Play(SOUND_LABEL_BGM000);
 
-	player.Init(L"asset/char01.png", 3, 4); //プレイヤーを初期化
-	player.SetPos(-100.0f, -154.0f, 0.0f);     //位置を設定
-	player.SetSize(45.0f, 60.0f, 0.0f);  //大きさを設定
-	player.SetAngle(0.0f);                 //角度を設定
-	player.SetColor(1.0f, 1.0f, 1.0f, 1.0f);
+	// プレイヤーの初期化
+	player.Init(L"asset/char01.png", 3, 4);
+	player.SetPos(-100.0f, -154.0f, 0.0f);
+	player.SetSize(45.0f, 60.0f, 0.0f);
 
-	player2.Init(L"asset/char01.png", 3, 4); //プレイヤーを初期化
-	player2.SetPos(-90.0f, -174.0f, 0.0f);     //位置を設定
-	player2.SetSize(40.0f, 20.0f, 0.0f);  //大きさを設定
-	player2.SetAngle(0.0f);                 //角度を設定
+	player2.Init(L"asset/char01.png", 3, 4);//右判定
+	player2.SetPos(-90.0f, -174.0f, 0.0f);
+	player2.SetSize(40.0f, 20.0f, 0.0f);
 	player2.SetColor(1.0f, 1.0f, 1.0f, 0.0f);
 
-	player3.Init(L"asset/char01.png", 3, 4); //プレイヤーを初期化
-	player3.SetPos(-110.0f, -164.0f, 0.0f);     //位置を設定
-	player3.SetSize(40.0f, 20.0f, 0.0f);  //大きさを設定
-	player3.SetAngle(0.0f);                 //角度を設定
+	player3.Init(L"asset/char01.png", 3, 4);//左判定
+	player3.SetPos(-110.0f, -164.0f, 0.0f);
+	player3.SetSize(40.0f, 20.0f, 0.0f);
 	player3.SetColor(1.0f, 1.0f, 1.0f, 0.0f);
+
+	player4.Init(L"asset/char01.png", 3, 4);//上判定
+	player4.SetPos(-100.0f, -134.0f, 0.0f);
+	player4.SetSize(40.0f, 20.0f, 0.0f);
+	player4.SetColor(1.0f, 1.0f, 1.0f, 0.5f);
+
+	player5.Init(L"asset/char01.png", 3, 4);//下判定
+	player5.SetPos(-100.0f, -194.0f, 0.0f);
+	player5.SetSize(10.0f, 5.0f, 0.0f);
+	player5.SetColor(1.0f, 1.0f, 0.0f, 1.0f);
+
+	// 背景やその他オブジェクトの初期化
+	background.Init(L"asset/stage.jpg");
+	background.SetPos(0.0f, 0.0f, 0.0f);
+	background.SetSize(640.0f, 480.0f, 0.0f);
+
+	scoreboard.Init(L"asset/number.png", 10, 1);
+	scoreboard.SetPos(-145.0f, 200.0f, 0.0f);
+	scoreboard.SetSize(40.0f, 40.0f, 0.0f);
 
 	title.Init(L"asset/Title.png");
 	title.SetPos(0.0f, 0.0f, 0.0f);
@@ -39,51 +55,46 @@ void Game::Init(HWND hWnd) {
 	pause.Init(L"asset/haikei.png");
 	pause.SetPos(0.0f, 0.0f, 0.0f);
 	pause.SetSize(640.0f, 480.0f, 0.0f);
-	pauseFg = false;
 
-	background.Init(L"asset/stage.jpg");
-	background.SetPos(0.0f, 0.0f, 0.0f);
-	background.SetSize(640.0f, 480.0f, 0.0f);
-	background.SetAngle(0.0f);
+	// ステージの床の初期化
 	for (int x = 0; x < STAGE_X; x++) {
 		for (int y = 0; y < STAGE_Y; y++) {
-			blocks[x][y].Init(L"asset/block.png");//ブロックを初期化
-			blocks[x][y].SetPos(BLOCK_SIZE * (x - STAGE_X / 2), y - STAGE_Y - 200, 00.0f);//位置を設定
-			blocks[x][y].SetSize(BLOCK_SIZE, BLOCK_SIZE, 0.0f);//大きさを設定
+			blocks[x][y].Init(L"asset/block.png");
+			blocks[x][y].SetPos(BLOCK_SIZE * (x - STAGE_X / 2) - 200, BLOCK_SIZE * y - 200, 0.0f);
+			blocks[x][y].SetSize(BLOCK_SIZE, BLOCK_SIZE, 0.0f);
 		}
 	}
-	for (int x = 0; x < YukaX; x++) {
-		for (int y = 0; y < YukaY; y++) {
-			blocks[x][y].Init(L"asset/block.png");  // ブロックを初期化
-			blocks[x][y].SetPos(BLOCK_SIZE * (x - YukaX / 2), y - YukaY - 130, 00.0f); // 位置を設定
-			blocks[x][y].SetSize(BLOCK_SIZE, BLOCK_SIZE, 0.0f); // 大きさを設定
-			blocks[x][y].SetAngle(0.0f);
+	//横ブロック
+	for (int x = 0; x < YokoX; x++) {
+		for (int y = 0; y < YokoY; y++) {
+			blocks[x][y].Init(L"asset/block.png");
+			blocks[x][y].SetPos(BLOCK_SIZE * (x - YokoX / 2) - 30, BLOCK_SIZE * (y - YokoY / 2) - 100, 0.0f);
+			blocks[x][y].SetSize(BLOCK_SIZE, BLOCK_SIZE, 0.0f);
 		}
 	}
-	
-	scoreboard.Init(L"asset/number.png", 10, 1);
-	scoreboard.SetPos(-145.0f, 200.0f, 0.0f);
-	scoreboard.SetSize(40.0f, 40.0f, 0.0f);
-	score = 0;
 
-	result.Init(L"asset/result1.png");
-	result.SetPos(0.0f, 0.0f, 0.0f);
-	result.SetSize(640.0f, 480.0f, 0.0f);
+	// 縦ブロックの初期化
+	for (int x = 0; x < TateX; x++) {
+		for (int y = 0; y < TateY; y++) {
+			blocks[x + YokoX][y].Init(L"asset/block.png");
+			blocks[x + YokoX][y].SetPos(BLOCK_SIZE * (x - TateX / 2), BLOCK_SIZE * (y - TateY / 2) - 130, 0.0f);
+			blocks[x + YokoX][y].SetSize(BLOCK_SIZE, BLOCK_SIZE, 0.0f);
+		}
+	}
+	tyu.Init(L"asset/krober.png");
+	tyu.SetPos(-5.0f, -135.0f, 0.0f); // 横ブロックと縦ブロックの交点に配置
+	tyu.SetSize(10.0f, 10.0f, 0.0f);
 
-	
-	
 }
 // 下にブロックがないときに落下を開始する関数
-		
+
+
 void Game::Update(void) {
 	input.Update();
 	switch (scene) {
 	case 0://タイトル画面
 		if (input.GetKeyTrigger(VK_3)) {
 			scene = 1;
-			
-			state = 0;
-			score - 0;
 		}
 		break;
 	case 1:
@@ -118,14 +129,23 @@ void Game::Update(void) {
 		//if (player.numU >= 3) {
 			//player.numU = 0;
 		//}
-		
+
 		// プレイヤーの処理
 		DirectX::XMFLOAT3 pos = player.GetPos();
 		DirectX::XMFLOAT3 pos2 = player2.GetPos(); // player2 の座標を取得
-		DirectX::XMFLOAT3 pos3 = player3.GetPos(); // player2 の座標を取得
-
+		DirectX::XMFLOAT3 pos3 = player3.GetPos(); // player3 の座標を取得
+		DirectX::XMFLOAT3 pos4 = player4.GetPos(); // player4 の座標を取得
+		DirectX::XMFLOAT3 pos5 = player5.GetPos(); // player5 の座標を取得
 		bool blockCollision = false; // player2 とブロックの接触フラグ
-		bool blockCollision2 = false; // player2 とブロックの接触フラグ
+		bool blockCollision2 = false; // player3 とブロックの接触フラグ
+		bool blockCollision3 = false; // player4 とブロックの接触フラグ
+		bool blockCollision4 = false; // player5とブロックの接触フラグ
+		if (pos.y <= -220.0f) {
+			// 初期位置に戻す
+			pos.x = -100.0f;
+			pos.y = -154.0f;
+			pos.z = 0.0f;
+		}
 		// player2 とブロックの衝突判定
 		for (int x = 0; x < STAGE_X; x++) {
 			for (int y = 0; y < STAGE_Y; y++) {
@@ -148,13 +168,25 @@ void Game::Update(void) {
 			}
 			if (blockCollision2) break;
 		}
+		//player4とブロックの衝突判定
+		for (int x = 0; x < STAGE_X; x++) {
+			for (int y = 0; y < STAGE_Y; y++) {
+				if (pos4.x >= blocks[x][y].GetPos().x && pos4.x <= blocks[x][y].GetPos().x + BLOCK_SIZE &&
+					pos4.y >= blocks[x][y].GetPos().y - BLOCK_SIZE && pos4.y <= blocks[x][y].GetPos().y) {
+					blockCollision3 = true;
+
+					break;
+				}
+			}
+			if (blockCollision3) break;
+		}
 		const int JUMP_COUNT = 30;  // ジャンプのカウント（最大カウント）
 		if (!jumpFg && !rakka) {
 			bool blockBelow = false; // 足元にブロックがあるかのフラグ
 			for (int x = 0; x < STAGE_X; x++) {
 				for (int y = 0; y < STAGE_Y; y++) {
-					if (pos.x >= blocks[x][y].GetPos().x && pos.x <= blocks[x][y].GetPos().x + BLOCK_SIZE &&
-						pos.y - 30.0f <= blocks[x][y].GetPos().y && pos.y - 30.0f >= blocks[x][y].GetPos().y - BLOCK_SIZE) {
+					if (pos5.x >= blocks[x][y].GetPos().x && pos5.x <= blocks[x][y].GetPos().x + BLOCK_SIZE &&
+						pos5.y <= blocks[x][y].GetPos().y && pos.y >= blocks[x][y].GetPos().y - BLOCK_SIZE) {
 						blockBelow = true;
 						break;
 					}
@@ -170,31 +202,36 @@ void Game::Update(void) {
 		if (input.GetKeyTrigger(VK_SPACE) && rakka == false && !jumpFg) {  // 落下していない場合にジャンプ開始
 			jumpFg = true;  // ジャンプ開始
 			count = 0;      // カウントをリセット
-
-			// YUKA_XとYUKA_Yを切り替え
-			if (YukaX == 8 && YukaY == 1) {
-				YukaX = 1; // YUKA_Xを1に
-				YukaY = 8; // YUKA_Yを10に
-			}
-			else {
-				YukaX = 8; // YUKA_Xを10に
-				YukaY = 1; // YUKA_Yを1に
-			}
-
-			// 地面の位置を更新
-			// ここで、変更されたYukaX, YukaYに基づいて地面ブロックの配置を更新
-			for (int x = 0; x < YukaX; x++) {
-				for (int y = 0; y < YukaY; y++) {
-					blocks[x][y].SetPos(BLOCK_SIZE * (x - YukaX / 2), BLOCK_SIZE * (y - YukaY / 2) - 130, 00.0f); // 新しい位置を設定
+			float right_x = blocks[YOKO_X - 1][0].GetPos().x; // 右端のx座標を基準
+			float right_y = blocks[YOKO_X - 1][0].GetPos().y; // 右端のy座標を基準
+			for (int x = 0; x < YOKO_X; x++) {
+				for (int y = 0; y < YOKO_Y; y++) {
+					float old_x = blocks[x][y].GetPos().x - right_x;
+					float old_y = blocks[x][y].GetPos().y - right_y;
+					float new_x = -old_y + right_x;
+					float new_y = old_x + right_y;
+					blocks[x][y].SetPos(new_x, new_y, 0.0f);
 				}
 			}
+
+			// 縦ブロックの回転
+			float top_x = blocks[YOKO_X - 1][0].GetPos().x; // 上端のx座標を基準
+			float top_y = blocks[YOKO_X - 1][0].GetPos().y; // 上端のy座標を基準
+			for (int x = 0; x < TATE_X; x++) {
+				for (int y = 0; y < TATE_Y; y++) {
+					float old_x = blocks[x + YOKO_X][y].GetPos().x - top_x;
+					float old_y = blocks[x + YOKO_X][y].GetPos().y - top_y;
+					float new_x = -old_y + top_x;
+					float new_y = old_x + top_y;
+					blocks[x + YOKO_X][y].SetPos(new_x, new_y, 0.0f);
+				}
+			}
+
 		}
 
 		if (jumpFg) {
 			if (count < JUMP_COUNT) {
 				pos.y += JUMP_HEIGHT; // player のジャンプ上昇
-				pos2.y = pos.y - 20.0f; // player2 のジャンプ上昇（player より -20）
-				pos3.y = pos.y - 20.0f; // player3 のジャンプ上昇（player より -20）
 				count++;
 			}
 			if (count == JUMP_COUNT) {
@@ -202,25 +239,19 @@ void Game::Update(void) {
 				rakka = true;   // 落下開始
 			}
 		}
-
-
-		if (rakka) {
+		if (rakka || blockCollision3) {
 			pos.y -= GRAVITY * 0.4f;  // player の重力による落下
-			pos2.y -= GRAVITY * 0.4f;
-			pos3.y -= GRAVITY * 0.4f;
-			pos2.y = pos.y - 20.0f;   // player2 の位置も player より -20 に設定
-			pos3.y = pos.y - 20.0f;   // player3 の位置も player より -20 に設定
+
 			// player1 の着地判定
 			bool blockBelow = false;
 			for (int x = 0; x < STAGE_X; x++) {
 				for (int y = 0; y < STAGE_Y; y++) {
-					if (pos.x >= blocks[x][y].GetPos().x && pos.x <= blocks[x][y].GetPos().x + BLOCK_SIZE &&
-						pos.y >= blocks[x][y].GetPos().y - BLOCK_SIZE && pos.y - 50.0f <= blocks[x][y].GetPos().y) {
+					if (pos5.x >= blocks[x][y].GetPos().x && pos5.x <= blocks[x][y].GetPos().x + BLOCK_SIZE &&
+						pos5.y >= blocks[x][y].GetPos().y - BLOCK_SIZE && pos5.y <= blocks[x][y].GetPos().y) {
 						blockBelow = true;
 						rakka = false;
-						pos.y = blocks[x][y].GetPos().y + 50.0f; // player の位置合わせ
-						pos2.y = pos.y - 20.0f; // player2 の位置を player から -20 に設定
-						pos3.y = pos.y - 20.0f; // player3 の位置を player から -20 に設定
+						pos5.y = blocks[x][y].GetPos().y; // player の位置合わせ
+
 						break;
 					}
 				}
@@ -231,23 +262,19 @@ void Game::Update(void) {
 				rakka = true; // 足元にブロックがなければ落下継続
 			}
 		}
-		// ブロック接触による左右制限の追加
-		
-		
+
 		// 横方向の移動制限
 		// A, D キーの処理制限
 		if (!blockCollision && input.GetKeyPress(VK_D)) {
-			pos.x += 0.6f; // player を右に移動
-			pos2.x += 0.6f; // player2 を右に移動
-			pos3.x += 0.6f; // player3 を右に移動
+			pos.x += 1.f; // player を右に移動
+
 		}
 
 		// Aキーの動作制限（左移動）
 		// Aキーの動作制限（左移動）
-		if(!blockCollision2 && input.GetKeyPress(VK_A)) {
-			pos.x -= 0.6f; // player を右に移動
-			pos2.x -= 0.6f; // player2 を右に移動
-			pos3.x -= 0.6f; // player3 を右に移動
+		if (!blockCollision2 && input.GetKeyPress(VK_A)) {
+			pos.x -= 1.0f; // player を右に移動
+
 		}
 		// player2 と player3 の x 座標を更新
 		pos2.x = pos.x + 10.0f; // player2 は player の x + 10
@@ -255,27 +282,41 @@ void Game::Update(void) {
 
 		pos3.x = pos.x - 10.0f; // player3 は player の x - 10
 		pos3.y = pos.y - 2.0f; // player3 は player の y - 10
+		pos4.x = pos.x - 0.0f; // player3 は player の x - 10
+		pos4.y = pos.y + 30.0f; // player3 は player の y - 10
+		pos5.x = pos.x - 0.0f; // player3 は player の x - 10
+		pos5.y = pos.y - 30.0f; // player3 は player の y - 10
 		// 画面外に行かないように位置を制限
 		if (pos.x < SCREEN_LEFT) pos.x = SCREEN_LEFT;
 		if (pos.x > SCREEN_RIGHT) pos.x = SCREEN_RIGHT;
 		if (pos.y < SCREEN_BOTTOM) pos.y = SCREEN_BOTTOM;
 		if (pos.y > SCREEN_TOP) pos.y = SCREEN_TOP;
-		
+
 		if (pos2.x < SCREEN_LEFT) pos2.x = SCREEN_LEFT;
 		if (pos2.x > SCREEN_RIGHT) pos2.x = SCREEN_RIGHT;
 		if (pos2.y < SCREEN_BOTTOM) pos2.y = SCREEN_BOTTOM;
 		if (pos2.y > SCREEN_TOP) pos2.y = SCREEN_TOP;
-		
+
 		if (pos3.x < SCREEN_LEFT) pos3.x = SCREEN_LEFT;
 		if (pos3.x > SCREEN_RIGHT) pos3.x = SCREEN_RIGHT;
 		if (pos3.y < SCREEN_BOTTOM) pos3.y = SCREEN_BOTTOM;
 		if (pos3.y > SCREEN_TOP) pos3.y = SCREEN_TOP;
-		
+
+		if (pos4.x < SCREEN_LEFT) pos4.x = SCREEN_LEFT;
+		if (pos4.x > SCREEN_RIGHT) pos4.x = SCREEN_RIGHT;
+		if (pos4.y < SCREEN_BOTTOM) pos4.y = SCREEN_BOTTOM;
+		if (pos4.y > SCREEN_TOP) pos4.y = SCREEN_TOP;
+
+		if (pos5.x < SCREEN_LEFT) pos5.x = SCREEN_LEFT;
+		if (pos5.x > SCREEN_RIGHT) pos5.x = SCREEN_RIGHT;
+		if (pos5.y < SCREEN_BOTTOM) pos5.y = SCREEN_BOTTOM;
+		if (pos5.y > SCREEN_TOP) pos5.y = SCREEN_TOP;
 		// 座標を設定
 		player.SetPos(pos.x, pos.y, pos.z);
 		player2.SetPos(pos2.x, pos2.y, pos2.z);
 		player3.SetPos(pos3.x, pos3.y, pos3.z);
-		
+		player4.SetPos(pos4.x, pos4.y, pos4.z);
+		player5.SetPos(pos5.x, pos5.y, pos5.z);
 	}
 	break;
 	case 2:
@@ -295,18 +336,31 @@ void Game::Draw(void) {
 		break;
 	case 1:
 	{
-		
+
 		background.Draw();
 		player.Draw();
 		player2.Draw();
 		player3.Draw();
+		player4.Draw();
+		player5.Draw();
+		tyu.Draw();
 		//ブロックを表示
 		for (int x = 0; x < STAGE_X; x++) {
 			for (int y = 0; y < STAGE_Y; y++) {
+				blocks[x][y].Draw(); // 各ブロックの描画を呼び出す
+			}
+		}
+		for (int x = 0; x < YokoX; x++) {
+			for (int y = 0; y < YokoY; y++) {
 				blocks[x][y].Draw();
 			}
 		}
-		
+
+		for (int x = 0; x < TateX; x++) {
+			for (int y = 0; y < TateY; y++) {
+				blocks[x + YokoX][y].Draw();
+			}
+		}
 		DirectX::XMFLOAT3 pos = scoreboard.GetPos();
 		DirectX::XMFLOAT3 size = scoreboard.GetSize();
 		int keta = 0;
@@ -335,15 +389,15 @@ void Game::Draw(void) {
 			scoreboard.Draw();
 			keta++;
 		} while (score >= (int)pow(10, keta));
-		
+
 		if (pauseFg == true) {
 			pause.Draw();
 		}
-		
+
 		break;
 
 	}
-	
+
 	D3D_FinishRender();
 }
 
@@ -351,13 +405,27 @@ void Game::Uninit(void) {
 	player.Uninit();
 	player2.Uninit();
 	player3.Uninit();
+	player4.Uninit();
+	player5.Uninit();
+	tyu.Uninit();
 	background.Uninit();
 	scoreboard.Uninit();
-	//ef.Uninit();
+
 	//ブロックを終了
 	for (int x = 0; x < STAGE_X; x++) {
 		for (int y = 0; y < STAGE_Y; y++) {
 			blocks[x][y].Uninit();
+		}
+	}
+	for (int x = 0; x < YokoX; x++) {
+		for (int y = 0; y < YokoY; y++) {
+			blocks[x][y].Uninit();
+		}
+	}
+
+	for (int x = 0; x < TateX; x++) {
+		for (int y = 0; y < TateY; y++) {
+			blocks[x + YokoX][y].Uninit();
 		}
 	}
 	title.Uninit();
